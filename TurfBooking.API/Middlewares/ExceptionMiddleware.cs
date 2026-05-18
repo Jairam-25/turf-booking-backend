74a26adc921cc24b9bbcb58e16bd6eac26483cc5
HP <jairam9003@gmail.com>
2026-05-18 16:06:25 +0530

    Serilog task-15

--- TurfBooking.API/Middlewares/ExceptionMiddleware.cs
+++ TurfBooking.API/Middlewares/ExceptionMiddleware.cs
@@ -1,8 +1,14 @@
-﻿namespace TurfBooking.API.Middlewares
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace TurfBooking.API.Middlewares
 {
-    public class ExceptionMiddleware(RequestDelegate next)
+    public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
     {
         private readonly RequestDelegate _next = next;
+        private readonly ILogger<ExceptionMiddleware> _logger = logger;
 
         public async Task InvokeAsync(
             HttpContext context)
@@ -13,12 +19,16 @@
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex,
+                    "Unhandled exception at {Path}",
+                    context.Request.Path);
+
                 context.Response.StatusCode = 500;
 
                 await context.Response.WriteAsJsonAsync(
                     new
                     {
-                        message = ex.ToString()
+                        message = "An unhandled exception occurred."
                     });
             }
         }
