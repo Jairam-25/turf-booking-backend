b1c43ce9d2137622324286cf8a03a188aef3937b
arunkumar0108 <kmarunkumarar0108@gmail.com>
2026-06-15 20:55:08 +0530

    fix(api): properly implement DummyLockFactory for Redis fallback compilation

--- TurfBooking.Infrastructure/Services/DummyLockFactory.cs
+++ TurfBooking.Infrastructure/Services/DummyLockFactory.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using RedLockNet;
@@ -12,6 +13,7 @@ namespace TurfBooking.Infrastructure.Services
         public string LockId => "dummy";
         public bool IsAcquired => true;
         public RedLockStatus Status => RedLockStatus.Acquired;
+        public RedLockInstanceSummary InstanceSummary => new RedLockInstanceSummary(1, 0, 0);
         public int ExtendCount => 0;
         public void Dispose() {}
         public ValueTask DisposeAsync() => ValueTask.CompletedTask;
@@ -19,8 +21,12 @@ namespace TurfBooking.Infrastructure.Services
 
     public class DummyLockFactory : IDistributedLockFactory
     {
+        public IRedLock CreateLock(string resource, TimeSpan expiryTime) => new DummyRedLock();
         public IRedLock CreateLock(string resource, TimeSpan expiryTime, TimeSpan waitTime, TimeSpan retryTime, CancellationToken? cancellationToken = null) => new DummyRedLock();
+        
+        public Task<IRedLock> CreateLockAsync(string resource, TimeSpan expiryTime) => Task.FromResult<IRedLock>(new DummyRedLock());
         public Task<IRedLock> CreateLockAsync(string resource, TimeSpan expiryTime, TimeSpan waitTime, TimeSpan retryTime, CancellationToken? cancellationToken = null) => Task.FromResult<IRedLock>(new DummyRedLock());
+        
         public void Dispose() {}
     }
 }
